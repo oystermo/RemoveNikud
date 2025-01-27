ad8616e691fa279101508c3f3c55db1b0561d73b
Nadav Mann Oryan <nmo1999@gmail.com>
2025-01-27 23:28:28 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SubtitleEdit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 13);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(968, 487);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Resize += new System.EventHandler(this.listView1_Resize);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Line #";
+            this.columnHeader1.Width = 48;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Before";
+            this.columnHeader2.Width = 303;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "After";
+            this.columnHeader3.Width = 335;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.Location = new System.Drawing.Point(777, 508);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(100, 28);
+            this.buttonOk.TabIndex = 3;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(885, 508);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(100, 28);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 521);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(82, 16);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Report Bugs";
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(1001, 546);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.listView1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(971, 577);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System.Globalization;
+using Nikse.SubtitleEdit.PluginLogic;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SubtitleEdit
+{
+    public partial class MainForm : Form
+    {
+        private readonly Subtitle _subtitle;
+        private const string from = "ְֱֲֳִֵֶַָֹֺֻּֽ֑֖֛֢֣֤֥֦֧֪֚֭֮֒֓֔֕֗֘֙֜֝֞֟֠֡֨֩֫֬֯־ֿ׀ׁׂ׃ׅׄ׆ׇ";
+
+        public string FixedSubtitle { get; private set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Escape)
+                    DialogResult = DialogResult.Cancel;
+                else if (e.KeyCode == Keys.Enter)
+                    OnClick(EventArgs.Empty);
+            };
+            listView1.Columns[2].Width = -2;
+            linkLabel1.Click += delegate { Process.Start("https://github.com/SubtitleEdit/plugins/issues/new"); };
+        }
+
+        public MainForm(Subtitle sub, string title, string description, Form parentForm)
+            : this()
+        {
+            Text = title;
+            _subtitle = sub;
+            GeneratePreview(false);
+        }
+
+        public override sealed string Text
+        {
+            get { return base.Text; }
+            set { base.Text = value; }
+        }
+
+        private void GeneratePreview(bool setText)
+        {
+            if (_subtitle == null)
+                return;
+            listView1.BeginUpdate();
+            foreach (Paragraph p in _subtitle.Paragraphs)
+            {
+                var before = Utilities.RemoveHtmlTags(p.Text, true);
+                var after = RemoveNikud(before);
+                AddToListView(p, before, after);
+                if (setText)
+                    p.Text = after;
+            }
+            listView1.EndUpdate();
+        }
+
+        private string RemoveNikud(string text)
+        {
+            string s = "";
+            for (int i=0; i<text.Length; i++)
+            {
+                if (!from.Contains(text[i] + "")){
+                    s = s + text[i];
+                }
+            }
+            return s;
+        }
+
+        private void AddToListView(Paragraph p, string before, string after)
+        {
+            var item = new ListViewItem(p.Number.ToString(CultureInfo.InvariantCulture)) { Tag = p };
+            item.SubItems.Add(before);
+            item.SubItems.Add(after);
+            listView1.Items.Add(item);
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            GeneratePreview(true);
+            FixedSubtitle = _subtitle.ToText(new SubRip());
+            DialogResult = DialogResult.OK;
+        }
+
+        private void listView1_Resize(object sender, EventArgs e)
+        {
+            var size = (listView1.Width - (listView1.Columns[0].Width)) >> 2;
+            listView1.Columns[1].Width = size;
+            listView1.Columns[2].Width = -2;
+        }
+
+    }
+}
